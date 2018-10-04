c3afbc158907e3547f1075c5309bc28650b6004a
philtd5000 <philiptaft9@gmail.com>
2018-10-03 18:52:37 -0700

    saved articles render sloppily

--- client/src/pages/Saved/Saved.js
+++ client/src/pages/Saved/Saved.js
@@ -1,6 +1,74 @@
 import React, { Component } from "react";
 import API from "../../utils/API";
-import { Link } from "react-router-dom";
+import MaterialGrid from "../../components/MaterialGrid";
+import styled from "react-emotion";
+// import { Link } from "react-router-dom";
+
+const OuterWrapper = styled('div')(
+    {
+        margin: 20,
+        borderRadius: 10,
+        border: '1px dashed white',
+        background: '#dce3e9'
+    }
+)
+
+const Title = styled('h2')(
+    {
+        fontSize: 24
+    }
+)
+
+const Article = styled('div')(
+    {
+        display: 'flex',
+        justifyContent: 'space-between',
+        borderTopStyle: 'double',
+        borderBottomStyle: 'double',
+        paddingTop: 10,
+        paddingBottom: 10,
+        borderTopColor: '#5972FF',
+        borderBottomColor: '#5972FF',
+        borderTopWidth: 8,
+        borderBottomWidth: 8,
+        margin: 40,
+        paddingLeft: 20,
+        background: 'white'
+    }
+)
+
+const ArticleContent = styled('div')(
+    {
+        flex: 9
+    }
+)
+
+const ArticleLink = styled('a')(
+    {
+        color: 'black'
+    }
+)
+
+const ButtonContainer = styled('div')(
+    {
+        display: 'flex',
+        alignItems: 'center',
+        flex: 1,
+    }
+)
+
+const Button = styled('button')(
+    {
+        background: '#dc3545',
+        borderRadius: 3,
+        border: 0,
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        textTransform: 'capitalize'
+    }
+)
 
 class Saved extends Component {
 
@@ -15,7 +83,7 @@ class Saved extends Component {
     loadSavedArticles = () => {
         API.getSavedArticles()
             .then(res =>
-                this.setState({ articles: res.data, title: "", date: "", url: "" })
+                this.setState({ articles: res.data })
             )
             .catch(err => console.log(err));
     };
@@ -28,8 +96,23 @@ class Saved extends Component {
 
     render() {
         return (
-            <div>Saved</div>
+            <MaterialGrid container justify="center">
+                <MaterialGrid item lg={10} md={10} sm={12} xs={12}>
+                    <OuterWrapper>
+                        {this.state.articles.map(article => (
+                            <Article key={article._id} id={article._id}>
+                                <ArticleContent>
+                                    <Title><ArticleLink href={article.url} target="_blank">{article.headline}</ArticleLink></Title>
+                                    <p>Published in {article.pubDate}</p>
+                                </ArticleContent>
+                            </Article>
+                        ))}
+                    </OuterWrapper>
+                </MaterialGrid>
+            </MaterialGrid>
         )
-      }
+    }
 
 }
+
+export default Saved;

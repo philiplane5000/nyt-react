bed36a9a5aeb610324b6af7d6406bd3334500db8
philtd5000 <philiptaft9@gmail.com>
2018-10-05 15:29:23 -0700

    sockets

--- sockets.js
+++ sockets.js
@@ -1,16 +1,18 @@
 
+const PORT = process.env.PORT || 8000;
+
 module.exports = (io) => {
-    io.on('connection', function(socket){
+    io.on('connection', function (socket) {
         console.log('a user connected');
-        socket.on('disconnect', function(){
-          console.log('User Disconnected');
+        socket.on('disconnect', () => {
+            console.log('User Disconnected');
         });
-        socket.on('savedArticle', function(title){
+        socket.on('savedArticle', (title) => {
             console.log(`"${title}" : SAVED`)
-          io.emit('article', title);
-          socket.broadcast.emit('article', title);
-        // return id + " saved";
+            io.emit('article', title);
+            socket.broadcast.emit('article', title);
+            // return id + " saved";
         });
-      });
-      io.listen(8000);
+    });
+    io.listen(PORT);
 }
